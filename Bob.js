e1ed321294c5f66dd20be8b3eda5cb90fec1e350
NBKeerthana <70811915+NBKeerthana@users.noreply.github.com>
2020-12-08 15:42:06 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,24 +1,23 @@
 class Bob{
-  constructor(x, y,radius) {
+  constructor(x,y,radius){
     var options = {
-      isStatic:false,
+      isStatic:true,
       restitution:0.3,
       friction:0.5,
       density:1.2
     }
-    this.body = Bodies.circle(x, y, radius, options);
-    this.radius=radius;
-    
-    World.add(world, this.body);
+    this.body = Bodies.circle(x,y,radius,options);
+    this.radius = radius;
+    World.add(world,this.body);
   }
-  display(){  
-    var pos =this.body.position;
+  display(){
+    var pos = this.body.position;
     var angle = this.body.angle;
     push();
-    translate(pos.x, pos.y);
+    translate(pos.x,pos.y);
     fill('pink');
     ellipseMode(RADIUS);
-    ellipse(this.body.position.x,this.body.position.y, this.radius, this.radius);
+    ellipse(0,0,this.radius,this.radius);
     pop();
   }
 }
