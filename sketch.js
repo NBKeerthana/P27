ddf5a5d441b85f6f1e4fd59f1333ba0b6c3027d5
NBKeerthana <70811915+NBKeerthana@users.noreply.github.com>
2020-12-08 15:44:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,9 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var roof1;
-var bob1;
+var roof;
+var bob1,bob2,bob3,bob4,bob5;
+var rope1,rope2,rope3,rope4,rope5;
 
 function preload()
 {
@@ -19,8 +20,15 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-	roof1 = new Roof(600,200,120,20);
-	bob1 = new Bob(600,400,20);
+	roof = new Roof(600,200,250,20);
+
+	bob1 = new Bob(520,400,20);
+	bob2 = new Bob(560,400,20);
+	bob3 = new Bob(600,400,20);
+	bob4 = new Bob(640,400,20);
+	bob5 = new Bob(680,400,20); 
+
+	rope1 = new Rope(bob1.body,roof.body,40*2 , 0);
 
 	Engine.run(engine);
   
@@ -31,9 +39,15 @@ function draw() {
   rectMode(CENTER);
   background('grey');
 
-  roof1.display();
-  bob1.display();
+  roof.display();
 
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+  rope1.display();
 
   drawSprites();
  
