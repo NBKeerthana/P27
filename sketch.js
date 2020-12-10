4cb54a5004ec3e6e934e6e93f188ed33a87e2c20
NBKeerthana <70811915+NBKeerthana@users.noreply.github.com>
2020-12-11 00:31:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
 var roof;
 var bob1,bob2,bob3,bob4,bob5;
@@ -28,7 +29,12 @@ function setup() {
 	bob4 = new Bob(640,400,20);
 	bob5 = new Bob(680,400,20); 
 
-	rope1 = new Rope(bob1.body,roof.body,40*2 , 0);
+	rope1 = new Rope(bob1.body,roof.body,-40*2 , 0);
+	rope2 = new Rope(bob2.body,roof.body,-20*2,0);
+	rope3 = new Rope(bob3.body,roof.body,0*2,0);
+	rope4 = new Rope(bob4.body,roof.body,20*2,0);
+	rope5 = new Rope(bob5.body,roof.body,40*2,0);
+
 
 	Engine.run(engine);
   
@@ -48,10 +54,19 @@ function draw() {
   bob5.display();
 
   rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
 
   drawSprites();
  
 }
 
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
 
-
+	  Matter.Body.applyForce(bob1.body,bob5.body.position,{x:60,y:0});
+  
+	}
+}
